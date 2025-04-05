f915fd895141107a2762950b8ffb9dc920a7235d
psatij01 <psatij01@students.cs.ubc.ca>
2025-04-05 16:59:22 -0700

    created ShareListEntry, WantListEntry and Item class

---
+++ src/main/java/com/trashpanda/ItemQuantityType.java
@@ -0,0 +1,25 @@
+/*
+ * Enum for quantity types.
+ */
+
+public enum ItemQuantityType {
+    COUNT,      
+    GRAM,       
+    KILOGRAM,
+    MILLILITER, 
+    LITER,
+    TSP,        
+    TBSP,       
+    CUP,
+    OZ,         
+    LB,         
+    PIECE,      
+    PACK,       
+    BOTTLE,
+    CAN,
+    BOX,
+    BAG,
+    JAR,
+    UNIT,       
+    NONE        
+}
