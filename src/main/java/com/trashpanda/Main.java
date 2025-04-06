9c1ae86b79f8eba77f4b809ef3c9310012f89e56
cwang766 <christinewang766@gmail.com>
2025-04-05 19:05:22 -0700

    added sharelist INSERT

--- src/main/java/com/trashpanda/Main.java
+++ src/main/java/com/trashpanda/Main.java
@@ -1,5 +1,11 @@
 package com.trashpanda;
+import java.util.Optional;
+import java.time.LocalDate;
 
 public class Main {
-
+        public static void main(String[] args) {
+//            Item item = new Item("paprika", ItemCategory.SPICES, ItemQuantityType.TBSP);
+//            ShareListService service = new ShareListService("christine", item, 1, Optional.of(LocalDate.of(2025, 6, 1)));
+//            service.insertShareListEntry();
+        }
 }
---
+++ src/main/java/com/trashpanda/ShareListController.java
@@ -0,0 +1,32 @@
+package com.trashpanda;
+import com.google.gson.Gson;
+import static spark.Spark.*;
+
+public class ShareListController {
+
+    public static void main(String[] args) {
+        port(8080);
+
+        post("/api/sharelist", (req, res) -> {
+            String body = req.body();
+            Gson gson = new Gson();
+            ShareListEntry shareListEntry = gson.fromJson(body, ShareListEntry.class);
+
+            ShareListService shareListService = new ShareListService();
+            boolean success = shareListService.insertShareListEntry(
+                    shareListEntry.getUsername(),
+                    shareListEntry.getItem(),
+                    shareListEntry.getQty(),
+                    shareListEntry.getExpirationDate()
+            );
+
+            if (success) {
+                res.status(200);
+                return "ShareList entry inserted successfully.";
+            } else {
+                res.status(500);
+                return "Error inserting ShareList entry.";
+            }
+        });
+    }
+}
